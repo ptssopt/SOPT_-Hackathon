567b21d6a8fe4a59570485e92f27e8a4f9304ba4
KimMooHyeon <dlfb77@gmail.com>
2019-05-05 17:15:15 +0900

    start sopt hackathon2

---
+++ app/src/main/java/com/computer/inu/sopt_hackathon_pts/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.computer.inu.sopt_hackathon_pts
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
